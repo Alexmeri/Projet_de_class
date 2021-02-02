5f1bf500ed1ea02ef210e68c864ec262c93b5fa5
Alexmeri <78029014+Alexmeri@users.noreply.github.com>
2021-02-02 10:40:02 -0500

    Create Class_main.py

---
+++ Class_main.py
@@ -0,0 +1,23 @@
+"""
+Alexander Meriakri
+gr 407
+Exercisse 2
+"""
+
+import random
+
+class NPC:
+    def ___init___(self):
+        self.force
+        self.agilite
+        self.constitution
+        self.intelligence
+        self.sagesse
+        self.charisme
+        self.armor_class = random.randint(1,13)
+        self.nom = ""
+        self.race = ""
+        self.espece = ""
+        self.life = random.randint(1,21)
+        self.proffesion = ""
+
